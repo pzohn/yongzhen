4401763f4c755c433350d494f1bd5757956d8a7a
pzohn <pzohn@163.com>
2019-01-02 19:27:02 +0800

    20190102-2

--- app/Http/Controllers/GoodController.php
+++ app/Http/Controllers/GoodController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Good;
 use App\Models\City;
+use App\Models\Leasing;
 
 class GoodController extends Controller
 {
@@ -42,6 +43,8 @@ class GoodController extends Controller
     public function getGood(Request $req) {
         $id = $req->get('id');
         $good = Good::GetGood($id);
+        $leasing_ids = $good->leasing_ids;
+        $pos = strpos($leasing_ids, '@');
         $goodTmp =[
             "name" => $good->name,
             "brand" => $good->brand,
@@ -53,8 +56,45 @@ class GoodController extends Controller
             "detail_pic" => $good->detail_pic,
             "price" => $good->price,
             "price_day" => $good->price_day,
-            "price_month" => $good->price_month
+            "price_month" => $good->price_month,
+            "leasing_pos" => $pos,
+            "leasing_first" => getLeasingFirst($leasing_ids),
+            "leasing" => getLeasing($leasing_ids)
         ];
         return $goodTmp;
+    }
+
+    public function getLeasingFirst($leasing_ids) {
+        $pos = strpos($leasing_ids, '@');
+        if ($pos == false){
+            $leasing = Leasing::GetLeasing($leasing_ids);
+            if ($leasing){
+                return $leasing;
+            }
+        }else{
+            $arry = preg_split("/@/",$trade->body);
+            $leasing = Leasing::GetLeasing($arry[0]);
+            if ($leasing){
+                return $leasing;
+            }
+        }
+    }
+
+    public function getLeasing($leasing_ids) {
+        $pos = strpos($leasing_ids, '@');
+        if ($pos == false){
+            $leasing = Leasing::GetLeasing($leasing_ids);
+            if ($leasing){
+                return $leasing;
+            }
+        }else{
+            $arry = preg_split("/@/",$trade->body);
+            $leasing = [];
+            foreach ($arry as $k => $v) {
+                $leasing[] = [
+                    Leasing::GetLeasing($v)
+                ];
+            }
+        }
     }
 }
