d23f50f5646c38fd577eb5a3b850fdeed01b1c4c
pzohn <pzohn@163.com>
2019-02-14 23:28:00 +0800

    20190214-1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -306,6 +306,7 @@ class UserController extends Controller
     }
 
     public function phoneVerify(Request $req) {
+        /*
         $data = [
             "sid" => "1cea456798e180e58fe1e96e9ef9a1e2",
             "token" => "2fe296ae9fb014aa261b93652375ad0b",
@@ -317,6 +318,70 @@ class UserController extends Controller
         $post_date = GuzzleHttp::options($data);
         $url = "https://open.ucpaas.com/ol/sms/sendsms";
         $result = GuzzleHttp::postJson($url, $post_date);
+        return $result;
+        */
+
+        $url = "https://open.ucpaas.com/ol/sms/sendsms";
+        $body_json = array(
+            "sid" => "1cea456798e180e58fe1e96e9ef9a1e2",
+            "token" => "2fe296ae9fb014aa261b93652375ad0b",
+            "appid" => "c3f231b11343405bb2a8956b2609205f",
+            "templateid" => "430629",
+            "param" => $req->get('param'),
+            "mobile" => $req->get('mobile')
+        );
+        $body = json_encode($body_json);
+        $data = $this->getResult($url, $body,'post');
+        return $data;
+    }
+
+    private function getResult($url, $body = null, $method)
+    {
+        $data = $this->connection($url,$body,$method);
+        if (isset($data) && !empty($data)) {
+            $result = $data;
+        } else {
+            $result = '没有返回数据';
+        }
+        return $result;
+    }
+
+    private function connection($url, $body,$method)
+    {
+        if (function_exists("curl_init")) {
+            $header = array(
+                'Accept:application/json',
+                'Content-Type:application/json;charset=utf-8',
+            );
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            if($method == 'post'){
+                curl_setopt($ch,CURLOPT_POST,1);
+                curl_setopt($ch,CURLOPT_POSTFIELDS,$body);
+            }
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+            $result = curl_exec($ch);
+            curl_close($ch);
+        } else {
+            $opts = array();
+            $opts['http'] = array();
+            $headers = array(
+                "method" => strtoupper($method),
+            );
+            $headers[]= 'Accept:application/json';
+            $headers['header'] = array();
+            $headers['header'][]= 'Content-Type:application/json;charset=utf-8';
+
+            if(!empty($body)) {
+                $headers['header'][]= 'Content-Length:'.strlen($body);
+                $headers['content']= $body;
+            }
+
+            $opts['http'] = $headers;
+            $result = file_get_contents($url, false, stream_context_create($opts));
+        }
         return $result;
     }
 }
