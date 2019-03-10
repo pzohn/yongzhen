65cbd09eb6bddc08dcad57127e78eebd6c62585d
pzohn <pzohn@163.com>
2019-03-10 17:33:36 +0800

    20190310-1

--- app/Http/Controllers/HospitalController.php
+++ app/Http/Controllers/HospitalController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Hospitaltype;
 use App\Models\City;
+use App\Models\Hospital;
 
 
 class HospitalController extends Controller
